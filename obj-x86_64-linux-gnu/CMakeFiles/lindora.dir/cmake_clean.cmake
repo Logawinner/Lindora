2bfeeb8dd5533576f1bfd0bf12059d97ff0c74b5
Logawinner <logawinner@gmail.com>
2026-06-11 03:27:20 -0400

    Release 1.1-1 Qt6 WebEngine Debian build

---
+++ obj-x86_64-linux-gnu/CMakeFiles/lindora.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lindora.dir/main.cpp.o"
+  "CMakeFiles/lindora.dir/main.cpp.o.d"
+  "lindora"
+  "lindora.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lindora.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ obj-x86_64-linux-gnu/CMakeFiles/lindora.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for lindora.
+# This may be replaced when dependencies are built.
